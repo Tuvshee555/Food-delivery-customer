8f59e0649def0e5a1d32b53f4ca179179f16e78f
Түвшинсайхан <tuvshee894@gmail.com>
2025-11-02 17:36:26 +0800

    location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,29 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
-"use client";
-
-import Link from "next/link";
+import { useState } from "react";
+import { AddLocation } from "./AddLocation";
 import { SheetRight } from "./SheetRight";
 import { Email } from "./Email";
-import { AddLocation } from "./AddLocation";
+import Link from "next/link";
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <>
-      <div className="w-screen h-[68px] py-[12px] px-[88px] flex justify-between">
-        <Link href={""} className="flex  items-center">
-          <img src="/order.png" className="w-[36px] h-[30px]" />
-          <div className="ml-2">
-            <div className="text-[18px] font-semibold">NomNom</div>
-            <div className="text-[12px] font-medium text-[#71717a]">
-              Swift delivery
-            </div>
+    <div className="w-screen h-[68px] py-[12px] px-[88px] flex justify-between">
+      <Link href={""} className="flex items-center">
+        <img src="/order.png" className="w-[36px] h-[30px]" />
+        <div className="ml-2">
+          <div className="text-[18px] font-semibold">NomNom</div>
+          <div className="text-[12px] font-medium text-[#71717a]">
+            Swift delivery
           </div>
-        </Link>
-        <div className="flex gap-[8px]">
-          <AddLocation
-            open={false}
-            onOpenChange={function (v: boolean): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-          <SheetRight />
-          <Email />
         </div>
+      </Link>
+
+      <div className="flex gap-[8px]">
+        <AddLocation open={open} onOpenChange={setOpen} />
+        <SheetRight />
+        <Email />
       </div>
-    </>
+    </div>
   );
 };
